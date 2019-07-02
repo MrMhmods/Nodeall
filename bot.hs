8a5c0b454e7f9c08c9934860bbacf92fa6625420
MrMhmods <52112618+MrMhmods@users.noreply.github.com>
2019-07-02 15:38:35 +0300

    Create bot.hs

---
+++ bot.hs
@@ -0,0 +1,33 @@
+
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+
+  console.log(`Logged in as ${client.user.tag}!`);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login("NTk1MTg2MzUzMzkwNzQ3NjUz.XRtQJA.AJKWHbsziwMlGJ-_y1Hpis8PF00");
+
